83b23b661f20fabf0fec399d1f142336fb524236
Rafael Garcia <rafbgarcia@gmail.com>
2013-09-22 03:54:03 -0300

    Removed field tags from Repository

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -6,8 +6,7 @@ class Repository
   field :repo_url, type: String
   field :avatar_url, type: String
   field :languages, type: Array
-  field :tags, type: Array
   field :url, type: String
-  
+
   has_and_belongs_to_many :tags
 end
