73dce99062ca533f18bf4dd68ac1e32341408072
Rafael Garcia <rafbgarcia@gmail.com>
2013-09-22 03:20:59 -0300

    +Henrique - Merged ProjectRepository with Repository

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,5 +1,10 @@
 class Repository
   include Mongoid::Document
+
   field :name, type: String
-  field :url, type: String
+  field :login, type: String
+  field :repo_url, type: String
+  field :avatar_url, type: String
+  field :languages, type: Array
+  field :tags, type: Array
 end
