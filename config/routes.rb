0a9e07719f2edd145f4bc736d6351060bc16a3ad
Max Lincoln <max@devopsy.com>
2013-09-21 23:48:27 -0300

    github login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Wondercode::Application.routes.draw do
+  devise_for :users, :path_names => {:sign_in => "login", :sign_out => "logout"},
+    :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+    get 'dashboard', :to => 'dashboard#index'
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
