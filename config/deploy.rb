67834c592d314d129a8626a44e493d2d13a308df
Max Lincoln <max@devopsy.com>
2013-10-06 20:43:26 -0500

    assigning MONGOLAB_URI and mongoid_user in different files didn't work

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,10 +13,6 @@ require 'capistrano/ext/multistage'
 
 require "bundler/capistrano"
 set :bundle_flags, "--deployment --quiet --binstubs"
-set :default_environment, {
-  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH",
-  'MONGOLAB_URI' => "mongodb://$mongoid_user:$mongoid_password@dbh46.mongolab.com:27467/wondercoders-dev"
-}
 
 role(:web) { domain }
 role(:app) { domain }
--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -5,3 +5,8 @@ set :user, "wondercode"
 set :mongoid_user, 'wondercode'
 set :mongoid_password, 'wondercode123'
 set :ssh_options, {:port => 2222, :forward_agent => true, keys: ['wondercode_demo_rsa']}
+
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH",
+  'MONGOLAB_URI' => "mongodb://#{mongoid_user}:#{mongoid_password}@dbh46.mongolab.com:27467/wondercoders-dev"
+}
